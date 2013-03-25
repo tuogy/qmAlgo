25a4cb89415e3cce2c17c207fcd9993b4ad908d8
moonL <guobichuan@gmail.com>
2013-03-26 01:55:53 +0800

    first edition

--- qm.rb
+++ qm.rb
@@ -1,21 +1,32 @@
 class QM
 ## Quine-McCluskey algorithm
-
+  attr_accessor :m, :d, :all, :level, :result, :essentials
   def initialize
-    
+    @result = []
+    @essentials = []
   end
 
 
   ## get input from keyborad like '3+5+8+10'
   ## return [3,5,8,10]
   def getInput
+    puts "input m(.): "
     string = gets
     string.strip!
-    res = string.split("+")
-    (0..res.size - 1).each do |i|
-      res[i] = res[i].to_i
+    mlist = string.split("+")
+    (0..mlist.size - 1).each do |i|
+      mlist[i] = mlist[i].to_i
     end
-    return res
+    @m = mlist.clone
+    puts "input d(.): "
+    string = gets
+    string.strip!
+    dlist = string.split("+")
+    (0..dlist.size - 1).each do |i|
+      dlist[i] = dlist[i].to_i
+    end
+    @d = dlist.clone
+    @all = @m + @d
   end
 
   
@@ -25,6 +36,7 @@ class QM
   ## ['xxxx',1] stands for it is prime
   def genFirstCol(minimum)
     level = Math.log2(minimum.max).floor + 1
+    @level = level
     res = Array.new(level + 1)
     (0..level).each {|i| res[i] = []}
     minimum.each do |i|
@@ -130,13 +142,75 @@ class QM
         res[i][j] = res[i][j].to_i(2)
       end
     end
+    @essentials = res
     return res
   end
   
+  #generate minimum not covered by essentials
+  def genMinOutOfEssentials(minimums)
+    res = @m.clone
+    @m.each do |i|
+      count = 0
+      index = nil
+      (0..minimums.size - 1).each do |j|
+        if(minimums[j].include?(i))
+          count += 1
+          index = j
+        end
+      end
+      if(count == 1)
+        @result << index
+        minimums[index].each do |toDelete|
+          res.delete(toDelete)
+        end
+      end
+    end
+    return res
+  end
+  
+  def bfsForCover(min,essens)
+    new_min = []
+    new_min <<  min
+    while true do
+      min = new_min.clone
+      new_min = []
+      count = 0
+      min.each do |i|
+        essens.each do |j|
+          if((i - j).empty?)
+            @result << count
+            return
+          end
+          count += 1
+          new_min << i - j
+        end
+      end
+    end
+  end
+  
+  def recoverSymbol
+    res = "A = "
+    @result.each do |i|
+      logic = ""
+      (0..@level - 1).each do |j|
+        if(@essentials[i].all?{|k| (k >> j) & 1 == 1})
+          logic = logic + "A#{j}"
+        elsif(@essentials[i].all?{|k| (k >> j) & 1 == 0})
+          logic = logic + "A#{j}\'"
+        end
+      end
+      res = res + (res.length == 4 ? '' : '+') +  logic
+    end
+    return res
+  end
+
 end
 
 qm = QM.new
-p min = qm.getInput
+min = qm.getInput
 firstCol = qm.genFirstCol(min)
-p allEssentials = qm.genAllEssentials(firstCol)
-p qm.symbol2Minimum(allEssentials)
+allEssentials = qm.genAllEssentials(firstCol)
+essens = qm.symbol2Minimum(allEssentials)
+min = qm.genMinOutOfEssentials(essens)
+qm.bfsForCover(min,essens)
+puts qm.recoverSymbol
